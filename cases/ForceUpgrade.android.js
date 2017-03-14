87fdf13957131771e772fd3a209ffdd4afcf2b38
jean <jean.ma.1986@gmail.com>
2017-03-14 17:00:58 +0800

    fix

--- cases/ForceUpgrade.android.js
+++ cases/ForceUpgrade.android.js
@@ -4,7 +4,8 @@
 import React,{Component} from 'react'
 import {
 	View,
-	Text
+	Text,
+	Alert
 } from 'react-native'
 import alivePush,{AlivePushStatus} from 'react-native-alive-push'
 
@@ -16,19 +17,46 @@ export default class ForceUpgrade extends Component {
 	constructor(props){
 		super(props);
 		this.state={
-			status:null
+			status:null,
+			err:"",
+			progress:""
 		}
 	}
-	alivePushStatusChange(status){
+	alivePushStatusChange(status,restart){
 		this.setState(Object.assign({},this.state,{
 			status
 		}));
+		if(status===AlivePushStatus.afterDownload && restart){
+			Alert.alert(
+				'更新',
+				'更新包已下载好,请重启进行更新!',
+				[
+					{text:'确定',onPress:()=>{restart();}}
+				],
+				{ cancelable: false }
+			);
+		}
 	}
+
+	alivePushDownloadProgress(received, total){
+		this.setState(Object.assign({},this.state,{
+			progress:received/total
+		}));
+	}
+
+	alivePushError(err){
+		this.setState(Object.assign({},this.state,{
+			err:err.toString()
+		}));
+	}
+
 	render() {
 		return (
 			<View style={{flexDirection:'column',alignItems:'center',flex:1,justifyContent:'center'}}>
-				<Text>Force Upgrade</Text>
+				<Text>检查到有更新能够正确下载,重启之后能够使用更新包进行安装</Text>
 				<Text>{this.state.status}</Text>
+				<Text>{this.state.progress}</Text>
+				<Text>{this.state.err}</Text>
 			</View>
 		);
 	}
