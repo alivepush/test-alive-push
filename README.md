de002d71fc0b56f19eea1a16b75a5493c65e6714
kunkunbobo <342317003@qq.com>
2018-06-11 09:43:43 +0800

    安卓环境配置

---
+++ README.md
@@ -0,0 +1,11 @@
+说明文档
+=============
+
+###第一步 安装
+> npm install
+
+###第二步 运行
+> ios : react-native run-ios --configuration Release
+---------------------------
+> android : react-native run-android --variant=release
+-------------------------
--- android/app/src/main/java/com/testalivepush/MainApplication.java
+++ android/app/src/main/java/com/testalivepush/MainApplication.java
@@ -7,7 +7,7 @@ import com.rnziparchive.RNZipArchivePackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.alivepush.RNAlivePushPackage;
-import com.reactlibrary.RNAlivePushModule;
+import com.alivepush.RNAlivePushModule;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -49,10 +49,6 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
             return Arrays.<ReactPackage>asList(
                     new MainReactPackage(),
-            new RNZipArchivePackage(),
-            new RNFetchBlobPackage(),
-            new RNDeviceInfo(),
-            new RNAlivePushPackage(),
                     new RNZipArchivePackage(),
                     new RNFetchBlobPackage(),
                     new RNDeviceInfo(),
